ff3613c94491e7c735f3438acb18aaef3405688b
chemso <bourabiachemseddine@gmail.com>
2025-04-17 21:38:36 +0100

    ready auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { EtudiantModule } from './etudiant/etudiant.module';
 import { EnseignantModule } from './enseignant/enseignant.module';
 import { AdministrateurModule } from './administrateur/administrateur.module';
@@ -8,6 +9,7 @@ import { ModulesModule } from './modules/modules.module';
 import { NotesModule } from './notes/notes.module';
 import { SchedulesModule } from './schedules/schedules.module';
 import { SectionModule } from './section/section.module';
+import { AuthModule } from './auth/auth.module';
 import { SectionController } from './section/section.controller';
 import { Administrateur } from './administrateur/administrateur.entity';
 import { Department } from './departments/departments.entity';
@@ -18,22 +20,42 @@ import { Note } from './notes/notes.entity';
 import { Schedule } from './schedules/schedules.entity';
 import { Section } from './section/section.entity';
 import { User } from './user.entity';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-  TypeOrmModule.forRoot({}),
-  TypeOrmModule.forFeature([
-    Administrateur,
-    Department,
-    Enseignant,
-    Etudiant,
-    StudyModule,
-    Note,
-    Schedule,
-    Section,
-    User
-  ]),
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [
+        Administrateur,
+        Department,
+        Enseignant,
+        Etudiant,
+        StudyModule,
+        Note,
+        Schedule,
+        Section,
+        User,
+      ],
+      synchronize: true,
+      ssl: { rejectUnauthorized: false },
+    }),
+    TypeOrmModule.forFeature([
+      Administrateur,
+      Department,
+      Enseignant,
+      Etudiant,
+      StudyModule,
+      Note,
+      Schedule,
+      Section,
+      User,
+    ]),
     EtudiantModule,
     EnseignantModule,
     AdministrateurModule,
@@ -42,7 +64,8 @@ import { ConfigModule } from '@nestjs/config';
     NotesModule,
     SchedulesModule,
     SectionModule,
+    AuthModule,
   ],
   controllers: [SectionController],
 })
-export class AppModule { }
+export class AppModule {}
